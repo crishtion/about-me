ce66e073adaee9c7af9e4d53313585fb3b4a8865
Crishtion <crishtionreyna248@gmail.com>
2024-12-11 10:19:54 -0600

    Update accodrian.js

--- accodrian.js
+++ accodrian.js
@@ -13,10 +13,10 @@ const checkInputs1  = () =>{
     count1 += 1
     console.log(count1)
     if(count1 >= 2){
-        element1.ariaChecked = false
+        element1.checked = false
         count1 = 0
         console.log('this button should close');
-    } else if(element2.ariaChecked === true || element3.ariaChecked === true){
+    } else if(element2.checked|| element3.checked){
         count1 = 0
         console.log(count1);
     }
@@ -25,10 +25,10 @@ const checkInputs2  = () =>{
     count2 += 1
     console.log(count2)
     if(count2 >= 2){
-        element1.ariaChecked = false;
+        element1.checked = false;
         count2 = 0
         console.log('this button should close');
-    } else if(element1.ariaChecked === true || element3.ariaChecked === true){
+    } else if(element1.checked || element3.checked){
         count2 = 0
         console.log(count2);
     }
@@ -37,10 +37,10 @@ const checkInputs3  = () =>{
     count3 += 1
     console.log(count3)
     if(count3 >= 2){
-        element1.ariaChecked = false;
+        element1.checked = false;
         count3 = 0
         console.log('this button should close');
-    } else if(element1.ariaChecked === true || element2.ariaChecked === true){
+    } else if(element1.checked  || element2.checked){
         count3 = 0
         console.log(count3)
     }
