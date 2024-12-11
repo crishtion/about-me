06dbae08c973035fceef43f9e26cc6394bd3b332
Crishtion <crishtionreyna248@gmail.com>
2024-12-11 09:55:06 -0600

    IS THIS THE DAGGER?

--- accodrian.js
+++ accodrian.js
@@ -8,30 +8,30 @@ let count3 = 0;
 const checkInputs1  = () =>{
     count1 += 1
     console.log('this button was clicked')
-    if(count1 = 2){
-        element1.checked = false;
+    if(count1 >= 2){
+        element1.ariaChecked = false;
         count1 = 0
-    } else if(element2.checked === true || element3){
+    } else if(element2.ariaChecked === true || element3.ariaChecked === true){
         count1 = 0
     }
 };
 const checkInputs2  = () =>{
     count2 += 1
     console.log('this button was clicked')
-    if(count2 = 2){
-        element1.checked = false;
+    if(count2 >= 2){
+        element1.ariaChecked = false;
         count2 = 0
-    } else if(element1.checked === true || element3.checked === true){
+    } else if(element1.ariaChecked === true || element3.ariaChecked === true){
         count2 = 0
     }
 };
 const checkInputs3  = () =>{
     count3 += 1
     console.log('this button was clicked')
-    if(count3 = 2){
-        element1.checked = false;
+    if(count3 >= 2){
+        element1.ariaChecked = false;
         count3 = 0
-    } else if(element1.checked === true || element2.checked === true){
+    } else if(element1.ariaChecked === true || element2.ariaChecked === true){
         count3 = 0
     }
 };
@@ -39,4 +39,6 @@ const checkInputs3  = () =>{
 
 element1.addEventListener('click', checkInputs1)
 element2.addEventListener('click', checkInputs2)
-element3.addEventListener('click', checkInputs2)
+element3.addEventListener('click', checkInputs3)
+
+
