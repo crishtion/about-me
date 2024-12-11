b22f1eedf0faea01a1b97a9e21d8f0efb62920ad
Crishtion <crishtionreyna248@gmail.com>
2024-12-11 10:10:42 -0600

    trying to get radio buttons to close

--- accodrian.js
+++ accodrian.js
@@ -5,37 +5,41 @@ let count1 = 0;
 let count2 = 0;
 let count3 = 0;
 
+console.log(count1)
+console.log(count2)
+console.log(count3)
+
 const checkInputs1  = () =>{
     count1 += 1
     console.log(count1)
     if(count1 >= 2){
-        element1.ariaChecked = false;
+        element1.ariaChecked = false
         count1 = 0
-        console.log('this button should close')
+        console.log('this button should close');
     } else if(element2.ariaChecked === true || element3.ariaChecked === true){
         count1 = 0
-        console.log(count1)
+        console.log(count1);
     }
 };
 const checkInputs2  = () =>{
     count2 += 1
-    console.log(count1)
+    console.log(count2)
     if(count2 >= 2){
         element1.ariaChecked = false;
         count2 = 0
-        console.log('this button should close')
+        console.log('this button should close');
     } else if(element1.ariaChecked === true || element3.ariaChecked === true){
         count2 = 0
-        console.log(count2)
+        console.log(count2);
     }
 };
 const checkInputs3  = () =>{
     count3 += 1
-    console.log(count1)
+    console.log(count3)
     if(count3 >= 2){
         element1.ariaChecked = false;
         count3 = 0
-        console.log('this button should close')
+        console.log('this button should close');
     } else if(element1.ariaChecked === true || element2.ariaChecked === true){
         count3 = 0
         console.log(count3)
