5fce71a1e41dc273f1dfee5da09c1ba1c4f46aba
Crishtion <crishtionreyna248@gmail.com>
2024-12-12 08:58:09 -0600

    Update accodrian.js 2

--- accodrian.js
+++ accodrian.js
@@ -18,7 +18,7 @@ const checkInputs1  = () =>{
 const checkInputs2  = () =>{
     count2 += 1
     if(count2 >= 2){
-        element2.checked = false;
+        element2.checked = false
         count2 = 0;
     } else if(element1.checked || element3.checked){
         count2 = 0
@@ -27,8 +27,8 @@ const checkInputs2  = () =>{
 const checkInputs3  = () =>{
     count3 += 1
     if(count3 >= 2){
-        element3.checked = false;
-        count3 = 0
+        element3.checked = false
+        count3 = 0;
     } else if(element1.checked  || element2.checked){
         count3 = 0
     }
